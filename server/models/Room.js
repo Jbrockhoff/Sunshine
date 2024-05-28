fc4f6e2ad1189068f10dce22f13c32964c380aa1
Jennie Brockhoff <jbrockh1@gmail.com>
2024-05-28 10:59:32 -0500

    added login logic and seeds. Needs debugging- issue validating password. Once fixed, server is finished

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,13 +1,39 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const roomSchema = new Schema({
   name: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   children: [{ type: Schema.Types.ObjectId, ref: 'Child' }],
+  password: {
+    type: String,
+    required: true
+  }
+}, 
+{
+  toJSON: {
+    virtuals: true
+  }
+}
+
+);
+
+roomSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
 });
+
+// This is a custom method to compare and validate password for logging in
+roomSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const Room = model('Room', roomSchema);
 
